331a7a04ec5151165e7cfb2334e6418410f5a45c
Dennis Muensterer <dennismuensterer@gmail.com>
2026-07-07 22:10:31 +0200

    Match clay config to docs

--- src/pkjs/index.js
+++ src/pkjs/index.js
@@ -1,27 +1,7 @@
-var Clay = require('./config');
+var Clay = require('@rebble/clay');
+var clayConfig = require('./config');
+var clay = new Clay(clayConfig);
 
 Pebble.addEventListener('ready', function() {
   console.log('Flipper watchface JS ready!');
-});
-
-Pebble.addEventListener('showConfiguration', function(e) {
-  Pebble.openURL(Clay.configUrl());
-});
-
-Pebble.addEventListener('webviewclosed', function(e) {
-  if (!e || !e.response) {
-    return;
-  }
-
-  var config = JSON.parse(decodeURIComponent(e.response));
-  console.log('Configuration received: ' + JSON.stringify(config));
-
-  Pebble.sendAppMessage(config,
-    function() {
-      console.log('Config sent to watchface');
-    },
-    function() {
-      console.log('Failed to send config to watchface');
-    }
-  );
 });
